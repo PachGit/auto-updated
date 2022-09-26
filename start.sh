0b1fe63469134aa3858ab2d19aae057a55f4c03b
PachGit <108862443+PachGit@users.noreply.github.com>
2022-09-26 10:45:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PachGit/auto-updated.git
+  git clone https://github.com/PachGit/auto-updated.git /auto-updated
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /auto-updated
 fi
-cd /choby
+cd /auto-updated
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ 𝗗𝗞 𝗕𝗢𝗧𝘅........"
 python3 bot.py
